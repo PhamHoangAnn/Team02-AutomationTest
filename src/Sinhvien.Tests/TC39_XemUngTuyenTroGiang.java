f01a20dfa2589bb8b22e9c0fbb16049dad0dd404
PhamHoangAnn <128809543+PhamHoangAnn@users.noreply.github.com>
2025-11-08 18:25:28 +0700

    Update TC39_XemUngTuyenTroGiang.java

--- src/Sinhvien.Tests/TC39_XemUngTuyenTroGiang.java
+++ src/Sinhvien.Tests/TC39_XemUngTuyenTroGiang.java
@@ -18,7 +18,7 @@ public class TC39_XemUngTuyenTroGiang {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.edge.driver", 
-            "C:\\Users\\asus\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+            "C:\\msedgedriver.exe");
         driver = new EdgeDriver();
         driver.manage().window().maximize();
 
@@ -70,3 +70,4 @@ public class TC39_XemUngTuyenTroGiang {
         }
     }
 }
+
